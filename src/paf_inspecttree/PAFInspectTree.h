0fcdf42889ef0e074b1d2d42bffef838a633af75
Javier Delgado <JavierDelgado@outlook.com>
2015-04-19 15:45:11 +0200

    Updated PAFInspectTree code. Tree specification behaviour changed.

--- src/paf_inspecttree/PAFInspectTree.h
+++ src/paf_inspecttree/PAFInspectTree.h
@@ -1,6 +1,13 @@
 #pragma once
 
-void printError();
-void printHelp();
-void printVariables(const char* fileName, const char* branchName, const char* treeName);
+#include "TTree.h"
+#include "TFile.h"
+
+void PrintMessage(const char* message);
+void PrintHelp();
+void PrintVariable(const char* type, const char* name);
+void PrintVariables(TTree* tree, const char* branchName);
+TTree* GetTree(TFile* rootFile, const char* treeName);
+TTree* GetAutoTree(TFile* rootFile);
+
 int main(int argc, const char* argv[]);
