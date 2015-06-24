322a1a97866a752d5ab3131a83671de72e70e4bc
Javier Delgado <JavierDelgado@outlook.com>
2015-06-25 00:13:26 +0200

    Added other possible types to PAFGenericItemVariableContainer template class.

--- src/variable_container/PAFGenericItemVariableContainerLinkDef.h
+++ src/variable_container/PAFGenericItemVariableContainerLinkDef.h
@@ -1,9 +1,19 @@
 #ifdef __CINT__
-#pragma link C++ class PAFGenericItemVariableContainer<bool>+;
-#pragma link C++ class PAFGenericItemVariableContainer<int>+;
-#pragma link C++ class PAFGenericItemVariableContainer<long>+;
-#pragma link C++ class PAFGenericItemVariableContainer<float>+;
-#pragma link C++ class PAFGenericItemVariableContainer<double>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Char_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<UChar_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Short_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<UShort_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Int_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<UInt_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Long_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<ULong_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Float_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Double_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Bool_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Byte_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Real_t>+;
+#pragma link C++ class PAFGenericItemVariableContainer<Text_t>+;
+
 #pragma link C++ class PAFGenericItemVariableContainer<TObject>+;
 #pragma link C++ class PAFGenericItemVariableContainer<TObject*>+;
 #pragma link C++ class PAFGenericItemVariableContainer<TString>+;
