d3e3d56b0db065e2f4544b19953038ca2d8289f4
Javier Delgado <JavierDelgado@outlook.com>
2015-06-26 00:26:36 +0200

    Added std::vector types to PAFVariableContainer.

--- src/variable_container/PAFGenericItemVariableContainerLinkDef.h
+++ src/variable_container/PAFGenericItemVariableContainerLinkDef.h
@@ -14,6 +14,21 @@
 #pragma link C++ class PAFGenericItemVariableContainer<Real_t>+;
 #pragma link C++ class PAFGenericItemVariableContainer<Text_t>+;
 
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Char_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<UChar_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Short_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<UShort_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Int_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<UInt_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Long_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<ULong_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Float_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Double_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Bool_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Byte_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Real_t>*>+;
+#pragma link C++ class PAFGenericItemVariableContainer<std::vector<Text_t>*>+;
+
 #pragma link C++ class PAFGenericItemVariableContainer<TObject>+;
 #pragma link C++ class PAFGenericItemVariableContainer<TObject*>+;
 #pragma link C++ class PAFGenericItemVariableContainer<TString>+;
