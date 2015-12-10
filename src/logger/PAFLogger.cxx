d9b8fb1aba8206694c01726aa91d5a1e2c91b771
Isidro Gonzalez Caballero <Isidro.Gonzalez.Caballero@cern.ch>
2015-12-10 19:11:44 +0100

    Moved PAFLogger into a proper singleton

--- src/logger/PAFLogger.cxx
+++ src/logger/PAFLogger.cxx
@@ -15,24 +15,34 @@
 #include "PAFStringUtil.h"
 
 //Default values
-PAFILogger* PAFLogger::fLogger = new PAFConsoleLogger();
-PAFLogLevel PAFLogger::fLogLevel = DEBUG;
+PAFLogger*  PAFLogger::fTheLogger = 0;
 
-void PAFLogger::Log(PAFLogLevel loglevel, const char* module, const char* msg)
-{
-	TString tmsg(msg);
-	if(!tmsg.Length())
-	{
-		return;
-	}
+PAFLogger::PAFLogger() {
+  // Default values for data members
+  fLogger = new PAFConsoleLogger();
+  fLogLevel = DEBUG;
+}
 
-	if (loglevel >= PAFLogger::fLogLevel)
-	{
-		PAFILogger* logger = GetLogger();
-		std::vector<TString*>* msgs = PAFStringUtil::Split(&tmsg, "\n");
-		for(unsigned int i = 0; i < msgs->size(); i++)
-		{
-			logger->Log(loglevel, module, msgs->at(i)->Data());
-		}
-	}
+PAFLogger::~PAFLogger() {
+  delete fLogger;
+}
+
+PAFLogger* PAFLogger::GetInstance() {
+  if (!fTheLogger)
+    fTheLogger = new PAFLogger;
+  return fTheLogger;
+}
+
+void PAFLogger::Log(PAFLogLevel loglevel, const char* module, const char* msg) {
+  TString tmsg(msg);
+  
+  if(!tmsg.Length()) return;
+  
+  if (loglevel >= PAFLogger::fLogLevel) {
+    PAFILogger* logger = GetLogger();
+    std::vector<TString*>* msgs = PAFStringUtil::Split(&tmsg, "\n");
+    for(unsigned int i = 0; i < msgs->size(); i++) {
+      logger->Log(loglevel, module, msgs->at(i)->Data());
+    }
+  }
 }
