9628d665261ccf44a72d7f44b56bddd98461906e
Isidro Gonzalez Caballero <Isidro.Gonzalez.Caballero@cern.ch>
2015-12-11 12:17:36 +0100

    Added colors to messages in the console. First implementation that needs probably further refinment

--- src/logger/PAFConsoleLogger.cxx
+++ src/logger/PAFConsoleLogger.cxx
@@ -11,7 +11,22 @@
 
 #include <iostream>
 
+// Colors for the different levels copied from https://root.cern.ch/doc/master/MsgLogger_8cxx_source.html
+static const char* PAFLogLevelColor[]   = { "\033[34m"      /*Blue       for DEBUG*/,
+					    ""              /*Nothing    for INFO*/,
+					    "\033[1;31m"    /*Red bold   for WARN*/,
+					    "\033[31m"      /*Red        for ERROR*/,
+					    "\033[37;41;1m" /*Gray & Red for FATAL*/};
+
 void PAFConsoleLogger::Log(PAFLogLevel level, const char * module, const char * msg)
 {
-	std::cout << PAFLogLevelStrings[level] << ": [" << module << "] " << msg << std::endl;
+        //std::cout << PAFLogLevelStrings[level] << ": [" << module << "] " << msg << std::endl;
+	std::cout 
+	  // Change color
+	  << PAFLogLevelColor[level]
+	  // Print text
+	  << PAFLogLevelStrings[level] << ": [" << module << "] " << msg 
+	  // Reset color
+	  << "\033[0m" 
+	  << std::endl;
 }
