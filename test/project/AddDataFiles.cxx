b9ddb7e686dc3f4139618871b0195e9756b8a40f
Isidro Gonzalez Caballero <Isidro.Gonzalez.Caballero@cern.ch>
2016-11-21 10:51:43 +0100

    GetDataFiles returns null pointer if no files have been added. Check that properly. Added also some debug information.

--- test/project/AddDataFiles.cxx
+++ test/project/AddDataFiles.cxx
@@ -29,20 +29,25 @@ void AddDataFiles::Test()
 	TList* elements = NULL;
 	PAFProject project;
 
-	AssertEquals(0, project.GetDataFiles()->GetNumOfFiles());
+	PAF_DEBUG("AddDataFiles","Initial check of empty Data project...");
+	AssertEquals((void*) 0, project.GetDataFiles());
 
+	PAF_DEBUG("AddDataFiles","Working with first root file (ROOT1)...");
 	project.AddDataFile("ROOT1.root");
 	elements = project.GetDataFiles()->GetListOfElements();
 	AssertEquals(1, elements->GetEntries());
 	AssertEquals("ROOT1.root", ((TDSetElement*)elements->At(0))->GetFileName());
 	AssertEquals("", ((TDSetElement*)elements->At(0))->GetObjName());
 
+	PAF_DEBUG("AddDataFiles","Working with second root file (ROOT2)...");
 	project.AddDataFile("ROOT2.root", "none");
 	elements = project.GetDataFiles()->GetListOfElements();
 	AssertEquals(2, elements->GetEntries());
 	AssertEquals("ROOT2.root", ((TDSetElement*)elements->At(1))->GetFileName());
 	AssertEquals("none", ((TDSetElement*)elements->At(1))->GetObjName());
 
+
+	PAF_DEBUG("AddDataFiles","Set default tree name...");
 	project.SetDefaultTreeName("tree");
 	elements = project.GetDataFiles()->GetListOfElements();
 	AssertEquals(2, elements->GetEntries());
