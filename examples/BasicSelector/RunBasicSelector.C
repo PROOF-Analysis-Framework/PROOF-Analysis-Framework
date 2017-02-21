82c12afdd153a0c179b1e5b1dfb2dba1e5502acd
Isidro González <Isidro.Gonzalez.Caballero@cern.ch>
2017-02-21 22:32:00 +0100

    changed path to file

--- examples/BasicSelector/RunBasicSelector.C
+++ examples/BasicSelector/RunBasicSelector.C
@@ -2,7 +2,7 @@
 void RunBasicSelector() 
 {
 	PAFProject* myProject = new PAFProject();
-	myProject->AddDataFile("/pool/ciencias/MC_Summer12_53X/Legacy/Tree_ZJets_Madgraph_0.root");
+	myProject->AddDataFile("/path/to/file.root");
 	myProject->AddSelectorPackage("BasicSelector");
 	myProject->Run();
 }
